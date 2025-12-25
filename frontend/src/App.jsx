2ade4a68dd90eb955842b744b2e5f61a94fb323a
Luann <luanndebortoli@gmail.com>
2025-12-25 19:05:18 -0300

    chore: vite structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import Board from './components/Board'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Board />
+    </>
+  )
+}
+
+
